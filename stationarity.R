2947cc436f718be0ee49398bbb9d4ce5d29333d1
Jan <janwohlfarth@gmx.de>
2024-08-27 09:44:17 +0200

    updates stationarity

--- stationarity.R
+++ stationarity.R
@@ -141,9 +141,49 @@ data <- data |>
         load = log(load)
     )
 
-# Log Transformation
+# Difference Smoothing (Trafo)
+# set calibration window in weeks
+N <- 5
+t_0 <- 168 * N + 1
+data_ds <- data.frame(x_t = data$load, date = data$date)
+
+data_ds$t1 <- 0
+data_ds$t2 <- 0
+data_ds$t3 <- 0
+data_ds$y <- NaN
+
+for (t in t_0:(t_0 + 1000)) {
+    # term_1
+    t1 <- 0
+    for (i in 1:(N - 1)) {
+        t1 <- t1 + data_ds$x_t[t - (i * 168)]
+    }
+    data_ds$t1[t] <- t1 / (N - 1)
+
+    # term 2
+    t2 <- 0
+    for (i in 1:7) {
+        t2 <- t2 + data_ds$x_t[t - (i * 24)]
+    }
+    data_ds$t2[t] <- t2 / 7
+
+    # term 3
+    t3 <- 0
+    for (i in 1:(N - 1)) {
+        for (j in 1:7) {
+            t3 <- t3 + data_ds$x_t[t - (i * 168) - (j * 24)]
+        }
+    }
+    data_ds$t3[t] <- t3 / (7 * (N - 1))
+
+    data_ds$y[t] <- data_ds$x_t[t] - data_ds$t1[t] - data_ds$t2[t] + data_ds$t3[t]
+}
+# Remove NaN values created by lagging
+data_ds <- na.omit(data_ds)
+
+# Log Transformation per hour mean
 data <- data |>
-    group_by(weekhour) |>
+    group_by(hour_int) |>
     mutate(
         load_origin = load,
         load = log(load),
@@ -161,32 +201,34 @@ for (i in 1:7) {
 
 data_fourier_day <- ts(data$load, frequency = 24)
 data_fourier_week <- ts(data$load, frequency = 24 * 7)
-data_fourier_year <- ts(data$load, frequency = 24 * 7 * 12)
+data_fourier_year <- ts(data$load, frequency = 24 * 365)
 fourier_terms_day <- fourier(data_fourier_day, K = 2)
 fourier_terms_week <- fourier(data_fourier_week, K = 2)
 fourier_terms_year <- fourier(data_fourier_year, K = 2)
 
-statio <- lm(data$load ~ data$weekhour + data$is_holiday + data$month_int)
 
-statio <- lm(data$load ~ data$weekday_int + data$is_holiday + data$hour_int + data$month_int)
+statio <- lm(data$load ~ data$is_holiday + fourier_terms_day + fourier_terms_week + fourier_terms_year + as.numeric(data$weekday_int))
 
 statio <- lm(diff(data$load, 1) ~ data$weekday_int[-1] + data$is_holiday[-1] + data$hour_int[-1] + data$month_int[-1])
+statio <- lm(data$load ~ data$weekhour + data$is_holiday + data$month_int)
 
 summary(statio)
 
 checkresiduals(statio)
-
 # Perform the Augmented Dickey-Fuller test for stationarity
 library(tseries)
-adf_test <- adf.test(statio$residuals)
+adf_test <- adf.test(first_1000_residuals)
 print(adf_test)
 
-# Extract the first 1000 residuals
-first_1000_residuals <- statio$residuals[1:1000]
+# Plot the fist n observations
+n <- 1000
+# Extract the first n residuals
+first_1000_residuals <- statio$residuals[1:n]
+first_1000_residuals <- data_ds$y
 
 # Create a data frame for plotting
 residuals_data <- data.frame(
-    index = data$date[1:1000],
+    index = data$date[1:n],
     residuals = first_1000_residuals
 )
 
@@ -201,7 +243,9 @@ p_residuals <- ggplot(residuals_data, aes(x = index, y = residuals)) +
 
 # Display the plot
 print(p_residuals)
-
+mean(first_1000_residuals)
+# Make it interactive
 library(plotly)
-
 ggplotly(p_residuals)
+
+acf(diff(first_1000_residuals))
